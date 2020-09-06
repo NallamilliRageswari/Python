fde2d6d0b8db7a94692e6be7fe2a5766b15f6262
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-09-06 22:07:37 +0530

    Add files via upload

---
+++ Alphabet_Pattern.py
@@ -0,0 +1,13 @@
+#Alphabet Pattern in this format.
+#ABCDE
+#ABCDE
+#ABCDE
+#ABCDE
+#ABCDE
+n=int(input("Enter a number : "))
+for i in range(n):
+    for j in range(65,70):
+        print(chr(j),end="")
+    print()
+    
+    
