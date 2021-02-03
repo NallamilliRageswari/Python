3d9524fd4577356d22a18750d2b54b9ef1111bff
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2021-02-03 22:04:27 +0530

    Add files via upload

---
+++ f_strings.py
@@ -0,0 +1,15 @@
+'''Using the concept of f-strings, print the following statement by substituting
+the ages:
+
+Riya's age is 5
+Mahesh is 10
+And their best friend, Arjun, is 12
+
+'''
+
+a=5
+b=10
+c=12
+print(f"Riya's age is {a}")
+print(f"Mahesh is {b}")
+print(f"And their best friend, Arjun, is {c}")
