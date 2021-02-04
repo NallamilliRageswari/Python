0ec637736590caa6c245fa36678fd7c48e465c8c
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2021-02-04 21:45:32 +0530

    Add files via upload

---
+++ Capitals_f_string.py
@@ -0,0 +1,12 @@
+'''
+Using the concept of f-strings, print the following statement by substituting
+the name of countries and capitals:
+
+New Delhi is the capital of India
+Madrid is the capital of Spain
+'''
+
+a='New Delhi'
+b='Madrid'
+print(f"{a} is the capital of India")
+print(f"{b} is the capital of Spain")
