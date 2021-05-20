2892f17897d508aca022a51125d0d8e5ef0c893a
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2021-05-20 11:32:22 -0400

    Add files via upload

---
+++ Russian_peasant_multiplication.py
@@ -0,0 +1,26 @@
+#using normal function
+def mul(a,b):
+    res=0
+    while True:
+        if(a%2!=0):
+            res+=b
+        a//=2
+        b*=2
+        if(a==0):
+            break
+    return res
+a,b=map(int,input().split())
+print(mul(a,b))
+
+ 
+#Using recursion
+def mul(a,b):
+    if a==0: #a==1 return b
+        return 0
+    if a%2:
+        return b+mul(a//2,b*2)
+    else:
+        return 0+mul(a//2,b*2)
+a,b=map(int,input().split())
+print(mul(a,b))
+
